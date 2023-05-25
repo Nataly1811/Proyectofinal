92d140cb77b05bfe8f27e854f35c6a8ec9ab7ebf
Nataly <nataly.atencio@gmail.com>
2023-05-25 19:40:37 -0300

    alertpaqueleo

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ if ((nombreUsuario == "Nataly") && (contrasenia == 12345)) {
 let numeroDedocumento = prompt ("Ingresar Numero de Documento")
 alert (" Bienvenido " +   entrada)*/
 
-let entrada1 = prompt("Ingresar Nombre")
+/*let entrada1 = prompt("Ingresar Nombre")
 let entrada2 = prompt ("Ingresar Edad")
 alert (" Hola " +   entrada1 )
 
@@ -21,4 +21,58 @@ while (entrada !="Voldemort")
 {
 clientes.push (entrada)
 entrada = prompt ("Ingresar Nombre")
+}*/
+
+/*function CostoProducto (tipo, precios) {
+        this.tipo;
+        this.precios;
+    }*/
+class Comidas {
+    constructor (tipo, Precio) {
+            this.tipo = tipo
+            this.precio = precio
 }
+Descuentos (){
+    let PorcentajeDeDescuento = Number (prompt("Ingresa tu porcentaje de descuento"))
+    let nuevoValor = this.precio - ((PorcentajeDeDescuento * this.precio)/ 100)
+    alert ("Con tu descuento la comida "+ "this.tipo"+ "te sale en" + "nuevoValor" + "dolares")
+
+}
+}
+    const comida1 = new Comidas("hamburguesaWopperSencilla", "3$")
+    const comida2 = new Comidas("hamburguesasPequeñas", "3$")
+    const comida3 = new Comidas("hamburguesaSencilla1contorno", "4$")
+    const comida4 = new Comidas("hamburguesaEspecial1contorno", "7$")
+    const comida5 = new Comidas("hamburguesaSencillaMaracucha", "4$")
+    const comida6 = new Comidas("hamburguesaEspecialMaracucha", "3$")
+  
+comida1.Descuentos ()
+
+
+/*for (const comida1 in object) {
+    if (Object.hasOwnProperty.call(object, comida1)) {
+        const element = object[comida1];
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+    }
+}   
+}*/
+
+
+/*class hamburguesa1 {
+    constructor(hamburguesaWopperSencilla, hamburguesasPequeñas, hamburguesaSencilla1contorno, hamburguesaEspecial1contorno, hamburguesaSencillaMaracucha, hamburguesaEspecialMaracucha, precio) {
+        this.hamburguesaWopperSencilla = hamburguesaWopperSencilla;
+        this.hamburguesasPequeñas = hamburguesasPequeñas;
+        this.hamburguesaSencilla1contorno = hamburguesaSencilla1contorno;
+        this.hamburguesaEspecial1contorno = hamburguesaEspecial1contorno;
+        this.hamburguesaSencillaMaracucha = hamburguesaSencillaMaracucha;
+        this.hamburguesaEspecialMaracucha = hamburguesaEspecialMaracucha;
+        this.precio
+}
+
+/* alert("la" + "this.hamburguesawopperSencilla" + "sale en" + "this.precios")
+alert("la" + "this.hamburguesaspequeñas" + "sale en "+ "this.precios")
+alert("la" + "this.hamburguesaSencilla1contorno" + "sale en" + "this.precios")
+alert("la" + "this.hamburguesaEspecial1contorno" + "sale en" + "this.precios")
+alert("la" + "this.hamburguesaSencillaMaracucha" + "sale en" + "this.precios")
+alert("la" + "this.hamburguesaEspecialMaracucha" + "sale en" + "this.precios")*/
